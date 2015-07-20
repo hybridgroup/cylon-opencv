68369e995beced2d2ee60a3d7893fa3705f2c8b2
Andrew Stewart <andrew@stwrt.ca>
2015-07-20 11:16:45 -0700

    clean up current version

--- examples/motion_detection/motion-detection.js
+++ examples/motion_detection/motion-detection.js
@@ -30,9 +30,10 @@ Cylon.robot({
       my.camera.on("motionDetected", function(err, im, rect) {
         if (err) { console.log(err); }
 
-        // display a square in the coordinates for the detected
-        // motion.
-        console.log(rect);
+        console.log("rect:", rect);
+
+        // draws a rectangle on the image, using the motion tracking coordinates
+        // im.rectangle([x, y], [x, y], [r, g, b], stroke-thickness)
         im.rectangle(
           [rect[0], rect[1]],
           [rect[2], rect[3]],
@@ -40,29 +41,24 @@ Cylon.robot({
           2
         );
 
-        // The second to last param is the color of the rectangle
-        // as an rgb array e.g. [r,g,b].
         // Once the image has been updated with rectangles around
         // the faces detected, we display it in our window.
         my.window.show(im, 40);
 
-        // After displaying the updated image we trigger another
+        // After displaying the updated image, we trigger another
         // frame read to ensure the fastest processing possible.
-        // We could also use an interval to try and get a set
-        // amount of processed frames per second, see below.
         my.camera.readFrame();
       });
 
       // We listen for frameReady event, when triggered
-      // we start the face detection passing the frame
+      // we start the motion detection passing the frame
       // that we just got from the camera feed.
       my.camera.on("frameReady", function(err, im) {
         if (err) { console.log(err); }
         my.camera.detectMotion(im, [420, 110, 490, 170]);
-        my.window.show(im, 40);
       });
 
-      every(50, my.camera.readFrame);
+      my.camera.readFrame();
     });
   }
 }).start();
